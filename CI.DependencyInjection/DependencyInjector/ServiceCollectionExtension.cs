a54b8dec1fdd2bd49fce8e47595ab88235e85278
Siarhei_Petrachenka <siarhei_petrachenka@epam.com>
2019-07-08 12:15:14 +0300

    Initial configuration.

---
+++ CI.DependencyInjection/DependencyInjector/ServiceCollectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CI.DependencyInjection.DependencyInjector
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddDependency(this IServiceCollection serviceCollection)
+        {
+            
+        }
+    }
+}
