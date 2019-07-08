11cbd2bbcaabb773b1a2709e09cb0ac50a9293a0
Siarhei_Petrachenka <siarhei_petrachenka@epam.com>
2019-07-08 16:07:56 +0300

    Added dataset properties.

--- CI.Data/Context/DomainContext.cs
+++ CI.Data/Context/DomainContext.cs
@@ -1,11 +1,36 @@
 ﻿using CI.Common.Helpers.DbContextHelpers;
 using CI.Data.Context.Configuration;
+using CI.Models.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CI.Data.Context
 {
     internal sealed class DomainContext : DbContext
     {
+        public DbSet<Attachment> Attachments { get; set; }
+
+        public DbSet<Board> Boards { get; set; }
+
+        public DbSet<Column> Columns { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public DbSet<TaskDates> TaskDates { get; set; }
+
+        public DbSet<TaskLabel> TaskLabels { get; set; }
+
+        public DbSet<TaskType> TaskTypes { get; set; }
+
+        public DbSet<TaskUsers> TaskUsers { get; set; }
+
+        public DbSet<Tempo> Tempi { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
         public DomainContext(DbContextOptions options) : base(options)
         {
             Database.EnsureCreated();
