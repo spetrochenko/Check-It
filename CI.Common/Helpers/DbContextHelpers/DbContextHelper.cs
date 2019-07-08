a6951cf9e378dacbde05a628a3375580c7327803
Siarhei_Petrachenka <siarhei_petrachenka@epam.com>
2019-07-08 14:30:01 +0300

    Datacontex initial configuration.

---
+++ CI.Common/Helpers/DbContextHelpers/DbContextHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace CI.Common.Helpers.DbContextHelpers
+{
+    public static class DbContextHelper
+    {
+        public static string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory() + "/Configs");
+
+            builder.AddJsonFile("connectionStringConfig.json");
+
+            var config = builder.Build();
+
+            return config.GetConnectionString("DefaultConnection");
+        }
+    }
+}
---
+++ CI.Data/Context/DomainContext.cs
@@ -0,0 +1,18 @@
+﻿using CI.Common.Helpers.DbContextHelpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace CI.Data.Context
+{
+    internal sealed class DomainContext : DbContext
+    {
+        public DomainContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbContextHelper.GetConnectionString());
+        }
+    }
+}
