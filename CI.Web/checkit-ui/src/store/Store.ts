e0019029d67a5244d8c717ca4451f1ef072a6ff1
Siarhei_Petrachenka <siarhei_petrachenka@epam.com>
2019-08-06 17:45:50 +0300

    Middleware was add.

--- CI.Web/checkit-ui/src/store/Store.ts
+++ CI.Web/checkit-ui/src/store/Store.ts
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers/RootReducer';
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../reducers/RootReducer";
+import thunk from "redux-thunk";
 
-const Store = createStore(rootReducer);
+const Store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default Store;
